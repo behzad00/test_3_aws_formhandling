9484bac2711bdcff00cccabf4a7257e2b60fe887
Behzad Pouyanfar <bzad0000@gmail.com>
2020-07-28 22:42:02 -0400

    initial commit

---
+++ app/app.js
@@ -0,0 +1,10 @@
+// Include the Main React Dependencies
+import React from "react"
+import ReactDOM from "react-dom"
+
+// Include the Main Component
+import Form from "./components/Form"
+// Include the Form Component
+
+// This code here allows us to render our main component (in this case Form)
+ReactDOM.render(<Form />, document.getElementById("app"));
